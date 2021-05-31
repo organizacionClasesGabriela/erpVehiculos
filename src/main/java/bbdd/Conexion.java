88e807c618543839237ab6d15922d55d26bb5fcb
gabi-mpg <gabrielamercadoperez@gmail.com>
2021-05-31 15:32:39 +0100

    Modificando la url de conexion para que no de el warning del SSL

--- src/main/java/bbdd/Conexion.java
+++ src/main/java/bbdd/Conexion.java
@@ -6,7 +6,7 @@ public class Conexion {
     //Conexion Local
     public static Connection conectar(){
         try{
-            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/erpvehiculos", "root", "");
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/erpvehiculos?useSSL=false", "root", "");
             return cn;
         }catch(SQLException e){
             System.err.println("Error en la conexión local " + e);
